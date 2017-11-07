3f32304672d5a991512465232cf01606711b2760
Christopher Stock <email@christopherstock.de>
2017-11-07 07:36:12 +0100

    Added state creation in Reducer.

--- js/redux/Reducer.jsx
+++ js/redux/Reducer.jsx
@@ -10,16 +10,16 @@
         /***************************************************************************************************************
         *   Specifies the global reducer method for the entire TaskList application.
         *
-        *   @param {Object} state  The existing state object.
+        *   @param {State}  state  The existing state object.
         *   @param {Object} action The action to perform on the state object.
         *
-        *   @return {Object} The new state object.
+        *   @return {State} The new state object.
         ***************************************************************************************************************/
-        static taskListReducer( state = Reducer.createDefaultState() , action )
+        static taskListReducer( state = new State(), action )
         {
             console.log( "Reducer.taskListReducer being invoked" );
             console.log( " applying action ", action );
-            console.log( " old state is ", state );
+            console.log( " old state is ",    state  );
 
             let newState = null;
 
@@ -62,60 +62,44 @@
         }
 
         /***************************************************************************************************************
-        *   Creates and returns the default state.
-        *
-        *   @return {Object} The initially constructed state object.
-        ***************************************************************************************************************/
-        static createDefaultState()
-        {
-            return {
-                taskList: [],
-            };
-        }
-
-        /***************************************************************************************************************
         *   Reduces the state in order to create a new task.
         *
-        *   @param {Object} state  The existing state object.
+        *   @param {State}  state  The existing state object.
         *   @param {Object} action The action to perform on the state object.
         *
-        *   @return {Object} The new and reduced state object.
+        *   @return {State} The new and reduced state object.
         ***************************************************************************************************************/
         static createTaskReducer( state, action )
         {
             let newTasks = state.taskList.slice();
             newTasks.push( action.taskName );
 
-            return {
-                taskList: newTasks,
-            };
+            return new State( newTasks );
         }
 
         /***************************************************************************************************************
         *   Reduces the state in order to delete a new task.
         *
-        *   @param {Object} state  The existing state object.
+        *   @param {State}  state  The existing state object.
         *   @param {Object} action The action to perform on the state object.
         *
-        *   @return {Object} The new and reduced state object.
+        *   @return {State} The new and reduced state object.
         ***************************************************************************************************************/
         static deleteTaskReducer( state, action )
         {
             let newTasks = state.taskList.slice();
             newTasks.splice( action.taskIndex, 1 );
 
-            return {
-                taskList: newTasks,
-            };
+            return new State( newTasks );
         }
 
         /***************************************************************************************************************
         *   Reduces the state in order to move a task up.
         *
-        *   @param {Object} state  The existing state object.
+        *   @param {State}  state  The existing state object.
         *   @param {Object} action The action to perform on the state object.
         *
-        *   @return {Object} The new and reduced state object.
+        *   @return {State} The new and reduced state object.
         ***************************************************************************************************************/
         static moveTaskUpReducer( state, action )
         {
@@ -126,18 +110,16 @@
             newTasks[ action.taskIndex - 1 ] = taskToMoveUp;
             newTasks[ action.taskIndex     ] = taskToMoveDown;
 
-            return {
-                taskList: newTasks,
-            };
+            return new State( newTasks );
         }
 
         /***************************************************************************************************************
         *   Reduces the state in order to move a task down.
         *
-        *   @param {Object} state  The existing state object.
+        *   @param {State}  state  The existing state object.
         *   @param {Object} action The action to perform on the state object.
         *
-        *   @return {Object} The new and reduced state object.
+        *   @return {State} The new and reduced state object.
         ***************************************************************************************************************/
         static moveTaskDownReducer( state, action )
         {
@@ -148,8 +130,6 @@
             newTasks[ action.taskIndex     ] = taskToMoveUp;
             newTasks[ action.taskIndex + 1 ] = taskToMoveDown;
 
-            return {
-                taskList: newTasks,
-            };
+            return new State( newTasks );
         }
     }
